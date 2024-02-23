5360d56d96aa9ee3b7b14358164cfafe54c6de73
Omar Emad <138994630+Om4r-007@users.noreply.github.com>
2024-02-23 13:43:49 +0300

    Update Predict_your_age.py

--- Python/Predict_your_age.py
+++ Python/Predict_your_age.py
@@ -1,1 +1,13 @@
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+    numbers = [age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8]
+    result = 0
+    
+    for num in numbers:
+        result += num ** 2
+    
+    result2 = result ** 0.5
+    result3 = result2 / 2
+    result4 = int(result3)
+    return result4 
 
+predict_age(65,60,75,55,60,63,64,45)
